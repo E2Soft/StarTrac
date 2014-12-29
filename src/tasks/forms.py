b593ecbb71240a268b02dd8460a31723840a2196
Milos Simic <milossimicsimo@gmail.com>
2014-12-29 23:08:54 +0100

    [NEW]Milestones list opcija prikaz svih milestonova

--- src/tasks/forms.py
+++ src/tasks/forms.py
@@ -3,3 +3,20 @@ Created on Dec 29, 2014
 
 @author: Milos
 '''
+from django.views.generic.list import ListView
+
+from tasks.models import Milestone
+
+
+class MilestonesList(ListView):
+    model = Milestone
+    template_name = 'tasks/milestones.html'
+    
+    def get_queryset(self):
+        return Milestone.objects.all()
+    
+    def get_context_data(self, **kwargs):
+        context = super(MilestonesList, self).get_context_data(**kwargs)
+        
+        context["back"] = self.request.META["HTTP_REFERER"]      
+        return context
--- src/tasks/urls.py
+++ src/tasks/urls.py
@@ -6,8 +6,10 @@ Created on Dec 21, 2014
 from django.conf.urls import patterns, url
 
 from src.tasks import views
+from tasks.forms import MilestonesList
 
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
+    url(r'^milestones/$', MilestonesList.as_view(), name='milestones'),
 )
