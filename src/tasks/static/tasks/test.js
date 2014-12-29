40d803709ad051363514e62fcfae9632198b30ab
Milos Simic <milossimicsimo@gmail.com>
2014-12-29 23:01:32 +0100

    [NEW] staticki fajloviu za jquery i nas js

---
+++ src/tasks/static/tasks/test.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+    $("#comment").click(function(){
+        $.ajax({
+            type: "GET",
+            url: "/tasks/mstnscmt/",
+            success: function(data){
+            var json = JSON.stringify(data);
+                alert(json);
+            },
+            error: function(){
+                alert("error");
+            }
+        });
+    });
+});
+
+$(document).ready(function(){
+	$("#form").submit(function(){
+        $.ajax({
+            type: "post",
+            dataType: "json",
+            data : {
+            	'hello':'wsaap',
+            	'csrfmiddlewaretoken' : $("input[name=csrfmiddlewaretoken]").val(),
+            	'content' : $("#content").val(),
+            	'pk' : $("#pk").val()
+            },
+            url: "/tasks/mstnscmt/",
+            success: function(data){
+            var json = JSON.stringify(data);
+                alert(json);
+                $("#rez").html(json);
+            },
+            error: function(){
+                alert("error");
+            }
+        });
+        return false;
+    });
+});
