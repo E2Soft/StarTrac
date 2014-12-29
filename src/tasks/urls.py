f62433915656251a5a0e7e1da3c95a985e75300e
Milos Simic <milossimicsimo@gmail.com>
2014-12-30 00:08:06 +0100

    [UPDATE] url i view

--- src/tasks/urls.py
+++ src/tasks/urls.py
@@ -5,7 +5,7 @@ Created on Dec 21, 2014
 '''
 from django.conf.urls import patterns, url
 
-from src.tasks import views
+from tasks import views
 from tasks.forms import MilestonesList, MilestoneDetail
 
 
@@ -13,4 +13,5 @@ urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     url(r'^milestones/$', MilestonesList.as_view(), name='milestones'),
     url(r'^mdetail/(?P<pk>\d+)/$', MilestoneDetail.as_view(), name='mdetail'),
+    url(r'^mcomment/$', views.mcomment, name='mcomment'),
 )
--- src/tasks/views.py
+++ src/tasks/views.py
@@ -1,4 +1,14 @@
+import json
+
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
+
+import tasks
+from tasks.models import Comment
+from tasks.models import Milestone
+
 
 # Create your views here.
 def index(request):
@@ -8,3 +18,18 @@ def index(request):
         return render(request,'tasks/logged.html',context)
     else:
         return render(request,'tasks/index.html')
+    
+def mcomment(request):
+    if request.POST:
+        content = request.POST.get("content","")
+        milestone_id = request.POST.get("pk","")
+        date = timezone.now()
+        milestone = get_object_or_404(Milestone,pk=milestone_id)
+        
+        """print("stigo post params: {}, {}, {}, {}, {}, {}, {}".format(hello, content, milestone_id, 
+                                                                 date, milestone, request.user, "K"))"""
+    
+    response_data = {}
+    response_data['result'] = 'failed'
+    response_data['message'] = 'You messed up'
+    return HttpResponse(json.dumps(response_data), content_type="application/json")
