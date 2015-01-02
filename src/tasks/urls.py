2e05891efdf2dbbd162bab52e308a23fb2e3f5b4
Milos Simic <milossimicsimo@gmail.com>
2015-01-02 03:40:28 +0100

    [NEW]Dodata statistika broj komentara za svaki milestone i zahtev [UPDATE] jos malo sredjen interfejs

--- src/tasks/urls.py
+++ src/tasks/urls.py
@@ -12,14 +12,16 @@ from tasks.forms import MilestonesList, MilestoneDetail, MilestoneUpdate, \
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
+    
     url(r'^milestones/$', MilestonesList.as_view(), name='milestones'),
     url(r'^mdetail/(?P<pk>\d+)/$', MilestoneDetail.as_view(), name='mdetail'),
-    url(r'^mcomment/$', views.mcomment, name='mcomment'),
     url(r'^medit/(?P<pk>\d+)/$', MilestoneUpdate.as_view(), name='medit'),
     url(r'^addmilestone/$', views.addmilestone, name='addmilestone'),
+    url(r'^mcomment/$', views.mcomment, name='mcomment'),
+    
     url(r'^requirements/$', RequirementsList.as_view(), name='requirements'),
     url(r'^rdetail/(?P<pk>\d+)/$', RequirementDetail.as_view(), name='rdetail'),
     url(r'^redit/(?P<pk>\d+)/$', RequirementUpdate.as_view(), name='redit'),
     url(r'^addrequirement/$', RequiremenCreate.as_view(), name='addrequirement'),
-     url(r'^rcomment/$', views.rcomment, name='rcomment'),
+    url(r'^rcomment/$', views.rcomment, name='rcomment'),
 )
